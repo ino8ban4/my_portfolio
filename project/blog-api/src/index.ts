636d615831cbbc30d65e06351b9f8d1b22a94591
ino8ban4 <dev.ino8ban4@gmail.com>
2026-04-22 08:56:07 +0900

    ci: add blog-api workflow and helm chart

--- project/blog-api/src/index.ts
+++ project/blog-api/src/index.ts
@@ -1,11 +1,27 @@
 import Fastify from 'fastify'
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 import { postRoutes } from './routes/posts'
 
 const app = Fastify({ logger: true})
 
-app.register(postRoutes)
 
 const start = async () => {
+  await app.register(swagger, {
+    openapi: {
+      info: {
+        title: 'Blog API',
+        version: '1.0.0'
+      }
+    }
+  })
+
+  await app.register(swaggerUi, {
+    routePrefix: '/docs'
+  })
+
+  app.register(postRoutes)
+
   try {
     await app.listen({ port: 3000, host: '0.0.0.0'})
   } catch (err) {
