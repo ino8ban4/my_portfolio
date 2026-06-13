e450989c1d2a0ac7f197457f9fd425f65d698645
ino8ban4 <dev.ino8ban4@gmail.com>
2026-06-13 16:49:38 +0900

    feat: Issue #26 エラーハンドリング（404,バリデーション追加

--- projects/blog-api/src/routes/posts.ts
+++ projects/blog-api/src/routes/posts.ts
@@ -8,28 +8,45 @@ export async function postRoutes(app: FastifyInstance) {
     return await prisma.post.findMany()
   })
 
+
   // 1件取得
-  app.get<{ Params: {id: string } }>('/posts/:id', async(request) => {
+  app.get<{ Params: {id: string } }>('/posts/:id', async(request, reply) => {
 
     const { id } = request.params;
 
-    return await prisma.post.findUnique({
+    const post = await prisma.post.findUnique({
       where: { id: Number(id) }
-      })
+    })
+    
+    if (!post){
+      return reply.code(404).send({ error: 'not found'})
+    }
+
+    return post
   })
 
+
   // データ登録
-  app.post<{ Body: { title: string, content: string} }>('/posts', async(request) => {
+  app.post<{ Body: { title: string, content: string} }>('/posts', async(request, reply ) => {
     const { title, content } = request.body;
+
+     if(!title || !content){
+      return reply.code(400).send({ error: 'title and content are required'})
+    }
 
     return await prisma.post.create({ data: {title, content } });
   });
 
+
   // データ更新
   app.put<{ Body: { title: string, content: string, published: boolean} , Params: { id: string } }>(
-    '/posts/:id', async(request) => {
+    '/posts/:id', async(request, reply ) => {
     const { title, content, published } = request.body;
     const { id } = request.params;
+
+    if(!title || !content){
+      return reply.code(400).send({ error: 'title and content are required'})
+    }
 
     return await prisma.post.update({
       where: { id: Number(id)},
@@ -37,6 +54,7 @@ export async function postRoutes(app: FastifyInstance) {
     })
   })
   
+
   // データ削除
   app.delete<{ Params: {id: string } }>('/posts/:id', async(request) => {
     const { id } = request.params;
