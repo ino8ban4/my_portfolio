459f7dda413db16f55c71d87c062b356b6c61695
ino8ban4 <dev.ino8ban4@gmail.com>
2026-06-23 08:21:19 +0900

    feat: Issue #32 削除機能実装（DELETE /posts/:id）

--- projects/blog/frontend/app/posts/[id]/page.tsx
+++ projects/blog/frontend/app/posts/[id]/page.tsx
@@ -1,3 +1,15 @@
+import { redirect } from 'next/navigation'
+
+async function deletePost(id: string) {
+  'use server'
+  
+  await fetch(`http://api:3000/posts/${id}`, {
+    method: 'DELETE',
+  })
+
+  redirect('/')
+}
+
 type Props = {
   params: Promise<{ id: string }>
 }
@@ -6,12 +18,19 @@ export default async function PostPage({ params }: Props) {
   const { id } = await params;
   const res = await fetch(`http://api:3000/posts/${id}`);
   const post = await res.json();
+  
+  const deletePostWithId = deletePost.bind(null, id)
+
   return (
     <main>
       <h1>{post.title}</h1>
       <p>{post.content}</p>
+
+      <form action={deletePostWithId}>
+      <button type="submit">削除</button>
+      </form>
+
     </main>
   )
 }
-
 
