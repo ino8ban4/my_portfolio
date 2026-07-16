0bf104f6fcb684d78c974a09a22cdccfa9f369a3
ino8ban4 <dev.ino8ban4@gmail.com>
2026-07-16 19:44:04 +0900

    fix: skip SSH/dotfiles setup in entrypoint.sh where dotfiles are unavailble(CI)

--- projects/blog/api/entrypoint.sh
+++ projects/blog/api/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-stow -d /root/dotfiles nvim tmux ghostty
-mkdir -p /root/.ssh
-echo "$(cat /root/.ssh_keys/authorized_keys)" >> /root/.ssh/authorized_keys
-chmod 700 /root/.ssh
-chmod 600 /root/.ssh/authorized_keys
-ssh-keygen -A
-echo "cd /workspace" >> /root/.profile
-echo "alias ll='ls -la'" >> /root/.profile
-/usr/sbin/sshd
+if [ -d /root/dotfiles/nvim ]; then
+  stow -d /root/dotfiles nvim tmux ghostty
+  mkdir -p /root/.ssh
+  echo "$(cat /root/.ssh_keys/authorized_keys)" >> /root/.ssh/authorized_keys
+  chmod 700 /root/.ssh
+  chmod 600 /root/.ssh/authorized_keys
+  ssh-keygen -A
+  echo "cd /workspace" >> /root/.profile
+  echo "alias ll='ls -la'" >> /root/.profile
+  /usr/sbin/sshd
+fi
 
 npm prisma generate
 
